36f647350120d4f7ddf48c1cbd3bc0995f795918
Mukul Gupta <maheshgupta067@gmail.com>
2018-06-04 16:55:48 +0530

    function

---
+++ 9.py
@@ -0,0 +1,26 @@
+def add(a, b):
+    c = a + b
+    return c
+result = add(100, 200)
+print(result)
+
+def mul(a, b):
+    print(a*b)
+mul(5, 5)
+mul(100, 200)
+
+def add(a, b):
+    return a+b
+def square(c):
+    return c*c
+
+result = square(add(3,3))
+print(result)
+
+import random
+for x in range(5):
+    result = random.randint(1,10)
+    print(result)
+
+
+
